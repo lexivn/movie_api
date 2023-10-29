747247f30370878aa31db496cd58813b2398806b
Alecks <52960889+lexivn@users.noreply.github.com>
2023-10-29 00:01:44 -0400

    Rollback Test Logging Auth

--- auth.js
+++ auth.js
@@ -14,7 +14,7 @@ let generateJWTToken = (user) => {
 
 /* POST login. */
 module.exports = (router) => {
-    router.post('/users/login', (req, res) => {
+    router.post('/login', (req, res) => {
         passport.authenticate('local', {session: false}, (error, user, info) => {
             if (error || !user) {
                 return res.status(400).json({
