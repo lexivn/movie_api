698a242ac8eb5947dfc80dccee29a8b0ed04e774
Alecks <52960889+lexivn@users.noreply.github.com>
2023-11-25 13:29:42 -0500

    Added enpoint to handle favorite movies list

--- index.js
+++ index.js
@@ -248,6 +248,32 @@ app.post("/users/:Username/movies/:MovieID", passport.authenticate('jwt', { sess
     });
 });
 
+// nEw
+// Allow users to update their user info (username, password, email, date of birth)
+app.put("/movie/setFavorite/:Username/:flag", passport.authenticate('jwt', { session: false }), async (req, res) => {
+  //Condition to Check Added Here
+  if (req.user.Username !== req.params.Username) {
+    return res.status(400).send('Permission denied');
+  }
+  
+  // Condition Ends
+  await Users.findOneAndUpdate(
+    { Username: req.params.Username },
+    {
+      $set: {
+        Feature: req.params.flag      }
+    },
+    { new: true }
+  ) // This line is used to make sure the updated document is returned
+    .then((updatedUser) => {
+      res.status(200).json(updatedUser);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    })
+});
+
 // DELETE (DELETE)
 // Allow users to delete movies from their favorite list
 app.delete("/users/:Username/movies/:MovieID", passport.authenticate('jwt', { session: false }), async (req, res) => {
@@ -289,6 +315,21 @@ app.delete("/users/:Username", passport.authenticate('jwt', { session: false }),
       } else {
         res.status(200).send(req.params.Username + " was deleted.");
       }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    });
+});
+
+app.get("/users/:Username", passport.authenticate('jwt', { session: false }), async (req, res) => {
+  //Condition to Check Added Here
+  if (req.user.Username !== req.params.Username) {
+    return res.status(400).send('Permission denied');
+  }
+  await Users.findOne({ Username: req.params.Username })
+    .then((user) => {
+      return res.status(200).send(user);
     })
     .catch((err) => {
       console.error(err);
