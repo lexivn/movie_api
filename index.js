9352739242ee8b1fa1e328f6e018d66449084160
Alecks <52960889+lexivn@users.noreply.github.com>
2023-11-27 23:58:51 -0500

    Deleted Get Singuler User Info and Feature prop setting
    Deleted the Get Singular User Info fetch and Feature prop setting from the movies.

--- index.js
+++ index.js
@@ -248,28 +248,27 @@ app.post("/users/:Username/movies/:MovieID", passport.authenticate('jwt', { sess
     });
 });
 
-// nEw
+// SET true or false the Feature property of the MOVIES.
 // Allow users to update their user info (username, password, email, date of birth)
-app.put("/movie/setFavorite/:movieId/:flag", passport.authenticate('jwt', { session: false }), async (req, res) => {
-  //Condition to Check Added Here
+// app.put("/movie/setFavorite/:movieId/:flag", passport.authenticate('jwt', { session: false }), async (req, res) => {
+//Condition to Check Added Here
   
-  // Condition Ends
-  await Movies.findOneAndUpdate(
-    { _id: req.params.movieId },
-    {
-      $set: {
-        Feature: req.params.flag      }
-    },
-    { new: true }
-  ) // This line is used to make sure the updated document is returned
-    .then((updatedUser) => {
-      res.status(200).json(updatedUser);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send("Error: " + err);
-    })
-});
+//   await Movies.findOneAndUpdate(
+//     { _id: req.params.movieId },
+//     {
+//       $set: {
+//         Feature: req.params.flag      }
+//     },
+//     { new: true }
+//   ) // This line is used to make sure the updated document is returned
+//     .then((updatedUser) => {
+//       res.status(200).json(updatedUser);
+//     })
+//     .catch((err) => {
+//       console.error(err);
+//       res.status(500).send("Error: " + err);
+//     })
+// });
 
 // DELETE (DELETE)
 // Allow users to delete movies from their favorite list
@@ -319,20 +318,21 @@ app.delete("/users/:Username", passport.authenticate('jwt', { session: false }),
     });
 });
 
-app.get("/users/:Username", passport.authenticate('jwt', { session: false }), async (req, res) => {
-  //Condition to Check Added Here
-  if (req.user.Username !== req.params.Username) {
-    return res.status(400).send('Permission denied');
-  }
-  await Users.findOne({ Username: req.params.Username })
-    .then((user) => {
-      return res.status(200).send(user);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send("Error: " + err);
-    });
-});
+// GET SINGULAR USER INFO
+// app.get("/users/:Username", passport.authenticate('jwt', { session: false }), async (req, res) => {
+//   //Condition to Check Added Here
+//   if (req.user.Username !== req.params.Username) {
+//     return res.status(400).send('Permission denied');
+//   }
+//   await Users.findOne({ Username: req.params.Username })
+//     .then((user) => {
+//       return res.status(200).send(user);
+//     })
+//     .catch((err) => {
+//       console.error(err);
+//       res.status(500).send("Error: " + err);
+//     });
+// });
 
 //app.listen(8080, () => console.log("Your app is listening on port 8080.")); 
 
